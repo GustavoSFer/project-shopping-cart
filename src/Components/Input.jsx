09f2f591e4617749c983523107eef7f7533928ce
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-08-05 16:57:37 -0300

    Feat: Validação email e senha - mensagem de erro

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Input({ type, name }) {
+function Input({
+  type, name, value, handleChange,
+}) {
   return (
-    <label htmlFor={name} className="input-group mb-3">
-      <input type={type} id={name} className="form-control" placeholder={name} />
+    <label htmlFor={name} className="input-group mt-3">
+      <input
+        type={type}
+        id={name}
+        onChange={handleChange}
+        value={value}
+        className="form-control"
+        placeholder={name}
+      />
     </label>
   );
 }
 
+Input.defaultProps = { value: '' };
 Input.propTypes = {
   type: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default Input;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../Components/Input';
-import '../App.css';
 import Button from '../Components/Button';
+import { isValidEmail, isValidPassword } from '../Utils/ValidaçãoLogin/ValidacaoLogin';
+import '../App.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [msgErro, setMsgErro] = useState(false);
+  const MIN_PASSWORD_LANGTH = 6;
+
   const handleClick = () => {
-    console.log('Botão de entrar foi clicado');
+    if (isValidEmail(email) && isValidPassword(MIN_PASSWORD_LANGTH, password)) {
+      alert('redirecionar para a proxima tela');
+    }
+    setMsgErro(true);
   };
 
   return (
     <main className="container-login container position-absolute top-50 start-50 translate-middle">
       <h1 className="txt-login">Login</h1>
       <div className="width-input-login">
-        <Input type="text" name="email" />
-        <Input type="password" name="password" />
+        <Input
+          type="text"
+          name="email"
+          handleChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <Input
+          type="password"
+          name="password"
+          handleChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <p className="min-password text-end">
+          {`Caracteres minimo para senha: ${MIN_PASSWORD_LANGTH}`}
+        </p>
         <Button click={handleClick}>Entrar</Button>
+        { msgErro && <p className="text-danger">E-mail ou senha incorreto!</p> }
       </div>
       <p className="txt-cadastro">
         Não possui cadastro?
