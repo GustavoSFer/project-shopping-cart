615a425d02d41837cf6e5b0f5ec27b19f34fd5d0
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-08-08 15:16:28 -0300

    Feat: Realizado layout do filtro quando aperta nele

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,6 +8,9 @@ import lupa from '../imagens/lupa.png';
 
 function Header() {
   const [pesquisar, setPesquisar] = useState('');
+  const [ativarFiltro, setAtivarFiltro] = useState(false);
+  const [filter, setFilter] = useState(0);
+
   return (
     <main>
       <div className="border border-success d-flex justify-content-between height">
@@ -30,7 +33,7 @@ function Header() {
         </div>
 
         <div className="d-flex align-items-center">
-          <Button click={() => ('fd')} sty="btn-filter-cart">
+          <Button click={() => setAtivarFiltro(!ativarFiltro)} sty="btn-filter-cart">
             <img src={filtro} alt="filtro" className="teste" />
           </Button>
           <Button click={() => ('fd')} sty="btn-filter-cart">
@@ -39,6 +42,84 @@ function Header() {
         </div>
 
       </div>
+      {console.log(filter)}
+      {
+        ativarFiltro && (
+          <div className="border border-success d-flex justify-content-evenly">
+            <div>
+              <select className="form-select">
+                <option defaultValue="selecione">= Selecionar =</option>
+                <option value="mais barato">Menor preço</option>
+                <option value="mais caro">Maior preço</option>
+              </select>
+            </div>
+
+            <div className="form-check">
+
+              <div className="d-flex flex-column">
+                Filtrar por valor:
+                <label htmlFor="filterPrice" className="form-check-label">
+                  <input
+                    type="radio"
+                    id="filterPrice"
+                    name="filterPrice"
+                    className="form-check-input"
+                    value="0-50"
+                    onChange={(e) => setFilter(e.target.value)}
+                  />
+                  Entre 0 e 50,00
+                </label>
+                <label htmlFor="filterPrice" className="form-check-label">
+                  <input
+                    type="radio"
+                    id="filterPrice"
+                    name="filterPrice"
+                    className="form-check-input"
+                    value="50-100"
+                    onChange={(e) => setFilter(e.target.value)}
+                  />
+                  Entre 50,00 e 100,00
+                </label>
+                <label htmlFor="filterPrice" className="form-check-label">
+                  <input
+                    type="radio"
+                    id="filterPrice"
+                    name="filterPrice"
+                    className="form-check-input"
+                    value="100-200"
+                    onChange={(e) => setFilter(e.target.value)}
+                  />
+                  Entre 100,00 e 200,00
+                </label>
+                <label htmlFor="filterPrice" className="form-check-label">
+                  <input
+                    type="radio"
+                    id="filterPrice"
+                    name="filterPrice"
+                    className="form-check-input"
+                    value="200-500"
+                    onChange={(e) => setFilter(e.target.value)}
+                  />
+                  Entre 200,00 e 500,00
+                </label>
+                <label htmlFor="filterPrice" className="form-check-label">
+                  <input
+                    type="radio"
+                    id="filterPrice"
+                    name="filterPrice"
+                    className="form-check-input"
+                    value="501"
+                    onChange={(e) => setFilter(e.target.value)}
+                  />
+                  Acima de 500
+                </label>
+              </div>
+            </div>
+
+          </div>
+        )
+      }
+
     </main>
   );
 }
